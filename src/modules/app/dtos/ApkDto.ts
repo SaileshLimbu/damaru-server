811a0be7ca97a1e82c5d98e46c00a412652b16b7
Dipesh Manandhar <dipesh.manandhar@itonics.de>
2025-01-30 07:25:32 +0545

    Apk versioning

--- src/modules/app/dtos/ApkDto.ts
+++ src/modules/app/dtos/ApkDto.ts
@@ -1,9 +1,10 @@
-import { IsBoolean } from 'class-validator';
+import { IsBoolean, IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ApkDto {
-  @ApiProperty({ type: 'string', format: 'binary', required: true })
-  file: Express.Multer.File;
+  @ApiProperty()
+  @IsNumber()
+  version: number;
 
   @ApiProperty()
   @IsBoolean()
